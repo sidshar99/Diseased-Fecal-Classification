bdf528b64bba5c078da21762298b895e130f2877
Siddharth Sharma <ss739@snu.edu.in>
2024-01-03 14:39:57 +0530

    Prepared Callbacks

--- src/CVClassifier/config/configuration.py
+++ src/CVClassifier/config/configuration.py
@@ -1,6 +1,8 @@
+import os
 from CVClassifier.constants import *
 from CVClassifier.entity.config_entity import DataIngestionConfig
 from CVClassifier.entity.config_entity import PrepareBaseModelConfig
+from CVClassifier.entity.config_entity import PrepareCallbacksConfig
 from CVClassifier.utils.common import read_yaml, create_directories
 
 class ConfigurationManager:
@@ -44,3 +46,19 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
